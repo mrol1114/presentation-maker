025175a57a7e0b02dea474252c16d6dcac5d96d0
Dmksm <gg323824@gmail.com>
2022-11-10 09:04:35 +0300

    сделал ImageComponent

--- src/slide/components/components/ImageComponent.tsx
+++ src/slide/components/components/ImageComponent.tsx
@@ -3,9 +3,8 @@ import type * as types from '../../../utils/types';
 
 function ImageComponent(prop: {imageElement: types.ImageInfo}): JSX.Element
 {
-    return (
-        <p>image</p>
-    );
+    return (prop.imageElement.type === "imageUrl" ? <img src={prop.imageElement.path}/> : 
+    <img src={"data:image/jpg;base64," + prop.imageElement.base64}/>);
 }
 
 export default ImageComponent;
