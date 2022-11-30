ff9ee9fb7a80446837659aeea8d0dfdc82506e01
Дмитрий Петухов <100488811+F4ntasy1@users.noreply.github.com>
2022-11-30 21:50:35 +0300

    поправил в названии ф-ии chnge->change

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/App';
-import { addOnChngeStateHandler } from "./actions/actions";
+import { addOnChangeStateHandler } from "./actions/actions";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -19,7 +19,7 @@ function render()
 }
 
 render();
-addOnChngeStateHandler(render);
+addOnChangeStateHandler(render);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
