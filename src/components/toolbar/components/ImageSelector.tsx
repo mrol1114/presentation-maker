f5f9272671be3f0c0d0cd2d9c8d0b962ab95536f
Дмитрий Петухов <100488811+F4ntasy1@users.noreply.github.com>
2023-01-01 21:11:27 +0300

    PM-39: сделал красную зловещую надпись при ошибке с картинкой по урлу в форме

--- src/components/toolbar/components/ImageSelector.tsx
+++ src/components/toolbar/components/ImageSelector.tsx
@@ -1,28 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { dispatch } from "../../../actions/actions";
 import * as functions from "../../../common/functions";
+import * as types from "../../../common/types";
 import styles from "./styles/styles.module.css";
 
-function ImageSelector(): JSX.Element
-{
-    const addImageBaseHandler = e =>
-    {
+function ImageSelector(prop: { isBackgroundImageSelector: boolean }): JSX.Element {
+    const [isError, setIsError] = useState(false);
+
+    const checkIfImageExists = (url: string, callback: Function) => {
+        const img = new Image();
+        img.src = url;
+
+        if (img.complete) callback(true);
+        else 
+        {
+            img.onload = () => {
+                callback(true);
+            };
+
+            img.onerror = () => {
+                callback(false);
+            };
+        }
+    }
+
+    const addImageBaseHandler = e => {
         const fileReader = new FileReader();
 
         fileReader.onload = () => {
-            dispatch(functions.addArea, {areaType: "imageBase64", path: fileReader.result});
+            const backgroundImage: types.ImageInfo = {
+                type: "imageBase64",
+                base64: fileReader.result as string
+            };
+
+            prop.isBackgroundImageSelector ? dispatch(functions.updateSlideProperty, { backgroundImage: backgroundImage }) :
+                dispatch(functions.addArea, { areaType: "imageBase64", path: fileReader.result });
+            
+            setIsError(false);
         }
         fileReader.readAsDataURL(e.target.files[0]);
     }
 
-    const addImageUrlHandler = () =>
-    {
-        const url: string|undefined = document.getElementById("selector")?.getElementsByTagName("input")[1].value;
-        dispatch(functions.addArea, {areaType: "imageUrl", path: url});
+    const addImageUrlHandler = () => {
+        const url: string | undefined = document.getElementById("selector")?.getElementsByTagName("input")[1].value;
+
+        const backgroundImage: types.ImageInfo = {
+            type: "imageUrl",
+            path: url as string
+        };
+
+        checkIfImageExists(url as string, (exists: boolean) => {
+            if (exists) 
+            {
+                setIsError(false);
+                prop.isBackgroundImageSelector ? dispatch(functions.updateSlideProperty, { backgroundImage: backgroundImage }) :
+                    dispatch(functions.addArea, { areaType: "imageUrl", path: url });
+            }
+            else
+            {
+                setIsError(true);
+            }
+        });
     }
 
-    const closeImageSelectorHandler = () => 
-    {
+    const closeImageSelectorHandler = () => {
         const selector = document.getElementById("image-selector");
         selector?.classList.remove(selector?.classList[1]);
     }
@@ -34,8 +75,9 @@ function ImageSelector(): JSX.Element
                     <div className={styles["image-base__text"]}>Загрузить с компьютера</div>
                     <input className={styles["image-base__input"]} type="file" accept=".jpg, .jpeg, .png" onChange={addImageBaseHandler} />
                 </label>
-                <input id="image-url" className={styles["image-url"]} placeholder="Добавьте URL изображения" type="url" autoComplete="off"/>
+                <input id="image-url" className={styles["image-url"]} placeholder="Добавьте URL изображения" type="url" autoComplete="off" />
                 <button className={styles["button-image-add"]} type="submit" onClick={addImageUrlHandler}>Добавить</button>
+                <a id="error-message" className={isError ? styles["error-message-active"] : styles["error-message-inactive"]}>Недействительный URL</a>
                 <button className={styles["button-ready"]} onClick={closeImageSelectorHandler}>Готово</button>
             </div>
         </div>
